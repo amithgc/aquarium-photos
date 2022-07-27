cab84e01d1d3216b76a764344e78176019c644c7
Amith Chandrappa <amith.chandrappa@leadsquared.com>
2022-07-27 16:49:52 +0530

    Script

---
+++ makeVideo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cd images
+#ffmpeg -framerate 10 -pattern_type glob -i '*.jpg' -vcodec libx264 -s 2048x1080 -pix_fmt yuv420p ../movie.mp4
+
+cd temp
+ffmpeg -framerate 10 -pattern_type glob -i '*.png' -vcodec libx264 -s 2048x1080 -pix_fmt yuv420p ../movie.mp4
