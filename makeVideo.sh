7f9fed82953f9acbb0b1c3b81372c642a7cc8ca3
Amith Chandrappa <amith.chandrappa@leadsquared.com>
2022-07-27 16:54:52 +0530

    Update makeVideo.sh

--- makeVideo.sh
+++ makeVideo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd images
-ffmpeg -framerate 10 -pattern_type glob -i '*.jpg' -vcodec libx264 -s 2048x1080 -pix_fmt yuv420p ../movie.mp4
+#ffmpeg -i * -c:v libx264 -crf 0 -y ../output.mp4
+
+ffmpeg -framerate 10 -pattern_type glob -i "*.jpg" -c:v libx264 -crf 0 ../movie-generic.mp4
+ffmpeg -framerate 10 -pattern_type glob -i '*.jpg' -vcodec libx264 -s 2048x1080 -pix_fmt yuv420p ../movie-apple.mp4
